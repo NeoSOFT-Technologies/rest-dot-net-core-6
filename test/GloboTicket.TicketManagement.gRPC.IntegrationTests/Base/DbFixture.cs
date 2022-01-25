bbbb2a77e69680d728ba3b0a0393747837be2a85
avivekm <92291258+avivekm@users.noreply.github.com>
2022-01-25 11:48:03 +0530

    PGSQL

--- test/GloboTicket.TicketManagement.gRPC.IntegrationTests/Base/DbFixture.cs
+++ test/GloboTicket.TicketManagement.gRPC.IntegrationTests/Base/DbFixture.cs
@@ -25,7 +25,7 @@ namespace GloboTicket.TicketManagement.gRPC.IntegrationTests.Base
 
 
             var dbProvider = Environment.GetEnvironmentVariable("dbProvider") != null
-                                ? Environment.GetEnvironmentVariable("dbProvider") : "MSSQL";
+                                ? Environment.GetEnvironmentVariable("dbProvider") : "PGSQL";
             switch (dbProvider)
             {
                 case "MSSQL":
