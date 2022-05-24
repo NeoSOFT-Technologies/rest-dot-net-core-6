bab4c8cdf8ce01cec7ebced292997ddadab0fe76
avivekm <vivek.maurya@neosoftmail.com>
2022-05-24 10:57:37 +0530

    reduced code smells

--- src/Core/GloboTicket.TicketManagement.Domain/Common/AuditableEntity.cs
+++ src/Core/GloboTicket.TicketManagement.Domain/Common/AuditableEntity.cs
@@ -4,9 +4,13 @@ namespace GloboTicket.TicketManagement.Domain.Common
 {
     public class AuditableEntity
     {
+#nullable enable
         public string? CreatedBy { get; set; }
+#nullable disable
         public DateTime CreatedDate { get; set; }
+#nullable enable
         public string? LastModifiedBy { get; set; }
         public DateTime? LastModifiedDate { get; set; }
+#nullable disable
     }
 }
--- src/gRPC/GloboTicket.TicketManagement.gRPC/Services/v1/CategoryService.cs
+++ src/gRPC/GloboTicket.TicketManagement.gRPC/Services/v1/CategoryService.cs
@@ -14,7 +14,7 @@ namespace GloboTicket.TicketManagement.gRPC.Services.v1
     {
         private readonly IMediator _mediator;
         private readonly IMapper _mapper;
-        readonly ILogger _logger;
+        //readonly ILogger _logger;
         public CategoryService(IMediator mediator, IMapper mapper)
         {
             _mediator = mediator;
