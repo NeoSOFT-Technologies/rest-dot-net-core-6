aa841c619d5205317b3a48d381c4e9320f16e37e
Apoorv Rane <apoorv.rane@neosoftmail.com>
2022-05-26 10:39:56 +0530

    Removed developer exception hotspot

--- src/gRPC/GloboTicket.TicketManagement.gRPC/Program.cs
+++ src/gRPC/GloboTicket.TicketManagement.gRPC/Program.cs
@@ -52,12 +52,6 @@ var app = builder.Build();
 // Configure the HTTP request pipeline.
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
-
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-
 AppContext.SetSwitch("Npgsql.EnableLegacyTimestampBehavior", true);
 app.UseRouting();
 app.UseAuthentication();
